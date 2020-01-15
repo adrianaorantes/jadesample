905b7f0d5938cfb9ec500b152acd7528324cbebb
ao2732 <ao2732@users.noreply.github.com>
2020-01-15 10:09:55 -0500

    tuesday

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 function changeColor(){
 var d1 = document.getElementById("div1");
 d1.className = "blue"; }
+function changeColor1(){
+var d2 = document.getElementById("div2");
+d2.className = "blue"; }
